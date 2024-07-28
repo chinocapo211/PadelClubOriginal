6922e7cddf202405ef1cca363bbd46c9486e8d50
FELIPE BLAUSTEIN <47807182@comunidadort.edu.ar>
2024-07-28 17:34:24 -0300

    se creo pagina de ajustes

--- src/screens/(home)/ajustes.js
+++ src/screens/(home)/ajustes.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import NavbarHigh from '../../components/navbarHigh';
+import NavbarLow from '../../components/navbarLow';
 const Ajustes = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Ajustes</Text>
+      <NavbarHigh/>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('MisClubes')}>
+        <Text style={styles.buttonText}>Mis Clubes</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('PreguntasFrecuentes')}>
+        <Text style={styles.buttonText}>Preguntas Frecuentes</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('TerminosCondiciones')}>
+        <Text style={styles.buttonText}>Términos y Condiciones</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('CerrarSesion')}>
+        <Text style={styles.buttonText}>Cerrar Sesión</Text>
+      </TouchableOpacity>
+      <NavbarLow/>
     </View>
   );
 };
@@ -14,6 +28,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#d3d3d3',
+    width: '75%',
+    height:'10%',
+    padding: '6%',
+    marginVertical: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 16,
   },
 });
 
