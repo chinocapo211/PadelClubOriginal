8f819b799e0a6867b5bc13579f6908bd0a533c36
Android Studio <devandroid@ort>
2024-08-02 10:05:06 -0300

    perfil

--- src/screens/(home)/perfil.js
+++ src/screens/(home)/perfil.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import NavbarHigh from '../../components/navbarHigh';
+import NavbarLow from '../../components/navbarLow';
 
 const Perfil = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Perfil</Text>
+      <NavbarHigh/>
+      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+        <Text style={styles.backButtonText}>{'<'}</Text>
+        </TouchableOpacity>
+      <NavbarLow/>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
+	const styles = StyleSheet.create({
+    container:{
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    height:'100%',
+  },
+  buttonContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: '80%',
+    maxWidth: 400,
+  },
+  button: {
+    height: 130,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginBottom: 10,
+    width: '100%', // Adjusted to '100%' to fit within parent container
   },
 });
 
