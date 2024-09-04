c43c4fd2990aeb7450b4c24808ed9265fe973777
Android Studio <devandroid@ort>
2024-09-04 09:05:14 -0300

    a

--- src/screens/(jugar)/puntajeJugar.js
+++ src/screens/(jugar)/puntajeJugar.js
@@ -1,39 +1,66 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import NavbarHigh from '../../components/navbarHigh';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
 const PuntajeJugar = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.safeArea}>
-    <View style={styles.container}>
-      <NavbarHigh/>
-      
-    </View>
+      <View style={styles.container}>
+        <NavbarHigh />
+        <View style={styles.scoreWrapper}>
+          <View style={styles.scoreContainer}>
+            <Text style={styles.scoreText}>Primer set</Text>
+            <Text style={styles.scoreText}>0 - 0</Text>
+          </View>
+          <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Cargar set</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   safeArea: {
-    flex:1,
+    flex: 1,
   },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  button: {
-    backgroundColor: '#d3d3d3',
-    width: '75%',
-    height:'10%',
-    padding: '6%',
-    marginVertical: 10,
+  scoreWrapper: {
+    backgroundColor: '#ffffff',
+    padding: 20,
+    borderRadius: 15,
+    marginBottom: 20,
+    width: '80%', // 80% del ancho de la pantalla
+  },
+  scoreContainer: {
+    flexDirection: 'column', // Cambiado a columna
+    justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 5,
+    width: '100%',
+    marginVertical: 20,
+  },
+  scoreText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#00bfff', // Color celeste
+    width: '100%',
+    height: '20%', // Más pequeño verticalmente
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
   },
   buttonText: {
     color: 'black',
     fontSize: 16,
+    textAlign:"center",
   },
 });
 
